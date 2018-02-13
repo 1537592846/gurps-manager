786e828f3bd820a451dd2af530922964ccbada0d
Ryuzaki <andrefranca86@gmail.com>
2018-02-12 20:50:38 -0400

    Updates

--- src/pages/character-status/character-status.ts
+++ src/pages/character-status/character-status.ts
@@ -10,32 +10,64 @@ import { Character } from '../../../models/Character';
 export class CharacterStatusPage {
 
   new_char: Character;
-  max_strenght:number;
+  max_strenght: number;
+  max_dexterity: number;
+  max_intelligence: number;
+  max_health: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    //Getting data
     this.new_char = this.navParams.get('new_char');
-    this.new_char.strenght=this.new_char.min_status;
-    this.new_char.dexterity=this.new_char.min_status;
-    this.new_char.intelligence=this.new_char.min_status;
-    this.new_char.health=this.new_char.min_status;
-    this.max_strenght=this.getMaxStatus("strenght");
+    //Setting values
+    this.new_char.current_points = 0;
+    //Setting minimun values
+    this.new_char.strenght = this.new_char.min_status * 1;
+    this.new_char.dexterity = this.new_char.min_status * 1;
+    this.new_char.intelligence = this.new_char.min_status * 1;
+    this.new_char.health = this.new_char.min_status * 1;
+    this.new_char.max_points = this.new_char.max_points * 1;
+    this.new_char.min_status = this.new_char.min_status * 1;
+    //Setting maximun values
+    this.updateStrenght();
+    this.updateDexterity();
+    this.updateIntelligence();
+    this.updateHealth();
   }
   goToCharacterLanguages() {
     this.navCtrl.push(CharacterLanguagesPage, { new_char: this.new_char });
   }
-  getMinStatus() {
-    return this.new_char.min_status;
+  updateStrenght() {
+    //Calculating current points
+    this.getCurrentPoints();
+    //Calculating max range
+    this.max_strenght = (this.new_char.max_points - this.new_char.current_points + (this.new_char.strenght - this.new_char.min_status) * 10) / 10 + this.new_char.min_status
+    console.log("st");
   }
-  getMaxStatus(range: string) {
-    switch (range) {
-      case "strenght":
-      case "health": {
-        console.log(((this.new_char.max_points / 10) + this.new_char.min_status));
-        return ((this.new_char.max_points / 10) + this.new_char.min_status);
-      }
-      default: {
-        return 0;
-      }
-    }
+  updateDexterity() {
+    //Calculating current points
+    this.getCurrentPoints();
+    //Calculating max range
+    this.max_dexterity = (this.new_char.max_points - this.new_char.current_points + (this.new_char.dexterity - this.new_char.min_status) * 10) / 10 + this.new_char.min_status
+    console.log("dx");
+  }
+  updateIntelligence() {
+    //Calculating current points
+    this.getCurrentPoints();
+    //Calculating max range
+    this.max_intelligence = (this.new_char.max_points - this.new_char.current_points + (this.new_char.intelligence - this.new_char.min_status) * 10) / 10 + this.new_char.min_status
+    console.log("iq");
+  }
+  updateHealth() {
+    //Calculating current points
+    this.getCurrentPoints();
+    //Calculating max range
+    this.max_health = (this.new_char.max_points - this.new_char.current_points + (this.new_char.health - this.new_char.min_status) * 10) / 10 + this.new_char.min_status
+    console.log("ht");
+  }
+  getCurrentPoints() {
+    this.new_char.current_points = (this.new_char.strenght - this.new_char.min_status) * 10;
+    this.new_char.current_points += (this.new_char.dexterity - this.new_char.min_status) * 20;
+    this.new_char.current_points += (this.new_char.intelligence - this.new_char.min_status) * 20;
+    this.new_char.current_points += (this.new_char.health - this.new_char.min_status) * 10;
   }
 }
