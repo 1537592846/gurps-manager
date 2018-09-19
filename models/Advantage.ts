66d29ef433366866dfebebe2d5f397aeed5742b2
Ryuzaki <andrefranca86@gmail.com>
2018-09-18 21:39:20 -0400

    Update

--- models/Advantage.ts
+++ models/Advantage.ts
@@ -1,38 +1,38 @@
 export class Advantage {
 
-    public id: number;
-    public name: string;
-    public description: string;
-    public types:string[];
-    public cost:number;
-    public level:number;
-    public levelCap:number;    
-    public formula: string;
+  public id: number;
+  public name: string;
+  public description: string;
+  public types: string[];
+  public cost: number;
+  public level: number;
+  public levelCap: number;
+  public formula: string;
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    isMental():boolean{
-      return this.types.find(x=>x=="mental")!=""
-      }
-    
-      isPhysical():boolean{
-        return this.types.find(x=>x=="physical")!=""
-      }
-    
-      isSocial():boolean{
-        return this.types.find(x=>x=="social")!=""
-      }
-    
-      isExotic():boolean{
-        return this.types.find(x=>x=="exotic")!=""
-      }
-    
-      isSupernatural():boolean{
-        return this.types.find(x=>x=="supernatural")!=""
-      }
-    
-      isMundane():boolean{
-        return this.types.find(x=>x=="mundane")!=""
-      }
+  isMental():boolean {
+    return this.types.find(x => x == "mental") != undefined
+  }
+
+  isPhysical(): boolean {
+    return this.types.find(x => x == "physical") != undefined
+  }
+
+  isSocial(): boolean {
+    return this.types.find(x => x == "social") != undefined
+  }
+
+  isExotic(): boolean {
+    return this.types.find(x => x == "exotic") != undefined
+  }
+
+  isSupernatural(): boolean {
+    return this.types.find(x => x == "supernatural") != undefined
+  }
+
+  isMundane(): boolean {
+    return this.types.find(x => x == "mundane") != undefined
+  }
 }
